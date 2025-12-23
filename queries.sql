985094b25d1d8c268ea3ba3e0f7cc97c746fc419
Md Sharafat Hassain <engsharafat23@gmail.com>
2025-12-24 01:08:11 +0600

    Add the readme file and also add the queries file with Joint

---
+++ queries.sql
@@ -0,0 +1,11 @@
+--JOINT--
+SELECT 
+  b.booking_id,
+  u.name AS customer_name,
+  v.name AS vehicle_name,
+  b.start_date,
+  b.end_date,
+  b.status
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN vehicles v ON b.vehicle_id = v.vehicle_id;
